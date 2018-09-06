57099b6bfb3b8765c33bf515e93ca77e22e7114f
avinean <avinean@gmail.com>
2018-09-06 23:47:31 +0300

    tmp

--- public_html/src/veiw/App.vue
+++ public_html/src/veiw/App.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 	#app
 		page-header(view='narrow' v-if='isHeader')
-		.main-block
+		.main-block(:style='styles')
 			router-view
 			callback(v-if='isCallback')
 			up(v-if='isUp')
@@ -21,9 +21,22 @@
 				isFooter: 1,
 				isCallback: 1,
 				isUp: 1,
+				styles: ''
 			}
 		},
 		mounted() {
+			if (this.$route.name === 'Admin') {
+				this.styles = `
+					    position: absolute;
+						top: 0;
+						right: 0;
+						bottom: 0;
+						left: 0;
+						margin: auto;
+						padding: 0;
+				`
+			}
+			
 			[].forEach.call(this.$children, e => {
 				if (e.viewOpts) {
 					for (let key in e.viewOpts) {
